6c725d279b0a10015718f5fbccda8c26673e40f4
Neider Lopez <lpzneider@gmail.com>
2023-11-01 19:51:00 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Start } from "./components/Start";
+
+function App() {
+  return (
+    <>
+      <Start />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Start/Start.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Start.scss";
+
+export type StartProps = {};
+
+const Start: React.FC<StartProps> = ({}) => {
+  return <div className="start">Start</div>;
+};
+
+export default Start;
